d7a24587d495bde4b990b994901c932cfe2778b0
jeffrey <ikechukwujeffrey19@gmail.com>
2023-03-31 11:22:32 +0100

    Fixed footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
@@ -7,11 +8,16 @@ const Footer = () => {
                 <div className="container">
                     <div className="logo">TJ Resources</div>
                     <div className="nav-links">
-                        <a href="">Home</a>
+                        {/* <a href="">Home</a>
                         <a href="">Services</a>
                         <a href="">About Us</a>
                         <a href="">Contact</a>
-                        <a href="">Admin Login</a>
+                        <a href="">Admin Login</a> */}
+                        <Link to='/'>Home</Link>
+                        <a href="#services">Services</a>
+                        <Link to='/about'>About Us</Link>
+                        <Link to='/contact'>Contact</Link>
+                        <a href="#">Admin Login</a>
                     </div>
                 </div>
                 <p className='copyright'>Copyright @ 2023</p>
